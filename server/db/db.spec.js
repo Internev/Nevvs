dfcdee6a5ccecc09a0a33646d3b580b9e5aa2093
Neville Neville <nchallinger@gmail.com>
2017-03-07 14:57:01 +1100

    Adjust db tests

--- server/db/db.spec.js
+++ server/db/db.spec.js
@@ -8,40 +8,66 @@ let feedlist = {
   htmlUrl: 'http://internev.com/'
 }
 
-describe('db', () => {
-  User.sync({force: true})
+test('db record creation', () => {
+  return User.sync({force: true})
   .then(() => {
-    return User.create({
+    User.create({
       email: 'test@eagle.com',
       password: 'hush',
       feeds: JSON.stringify(feedlist)
     })
-  })
-  .then(() => {
-    User.findAll()
-      .then(users => {
-        it('should write to db', () =>{
-          expect(users[0].email).toBe('test@eagle.com')
-        })
-      })
-  })
-  .then(() => {
-    User.findOne({email: 'test@eagle.com'})
-      .then(user => {
-        it('should find one in db', () => {
-          expect(user.email).toBe('test@eagle.com')
-        })
-      })
-  })
-  .then(() => {
-    User.destroy({where: {email: 'test@eagle.com'}})
-  })
-  .then(() => {
-    User.findAll()
-      .then(users => {
-        it('should write to db', () =>{
-          expect(users.length).toBe(0)
-        })
-      })
+    .then(() => {
+      return User.findAll()
+    })
+    .then(users => {
+      expect(users[0].email).toBe('test@eagle.com')
+    })
+    .then(() =>{
+      return User.findOne({email: 'test@eagle.com'})
+    })
+    .then(user => {
+      expect(user.email).toBe('test@eagle.com')
+    })
   })
 })
+
+//
+// describe('db', () => {
+//   // let findAllResult, findOneResult
+//   User.sync({force: true})
+//   .then(() => {
+//     User.create({
+//       email: 'test@eagle.com',
+//       password: 'hush',
+//       feeds: JSON.stringify(feedlist)
+//     })
+//   })
+//   .then(() => {
+//     User.findAll()
+//       .then(users => {
+//         // findAllResult = users[0].email
+//         it('should write to db', () => {
+//           expect(users[0].email).toBe('test@eagle.com')
+//         })
+//       })
+//   })
+//   .then(() => {
+//     User.findOne({email: 'test@eagle.com'})
+//       .then(user => {
+//         it('should find one in db', () => {
+//           expect(user.email).toBe('test@eagle.com')
+//         })
+//       })
+//   })
+//   .then(() => {
+//     User.destroy({where: {email: 'test@eagle.com'}})
+//   })
+//   .then(() => {
+//     User.findAll()
+//       .then(users => {
+//         it('should write to db', () => {
+//           expect(users.length).toBe(0)
+//         })
+//       })
+//   })
+// })
